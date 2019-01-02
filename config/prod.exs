9396a17ecea89dd3a668a7f466b3f2ad218e660a
Rodrigo Zampieri Castilho <rodrigo.zampieri@gmail.com>
2019-01-02 11:30:04 -0200

    Add :adapter configuration

--- config/prod.exs
+++ config/prod.exs
@@ -10,6 +10,7 @@ config :mockatron, MockatronWeb.Endpoint,
 config :logger, level: :info
 
 config :mockatron, Mockatron.Repo,
+  adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   ssl: true
