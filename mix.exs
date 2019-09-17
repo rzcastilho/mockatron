1d0d905a8df8ceea0a521a0749d6aa7696f62508
Rodrigo Zampieri Castilho <rodrigo.zampieri@gmail.com>
2019-09-17 17:04:12 -0300

    Upgrade bcrypt_elixir and adjust bcrypt function calls

--- mix.exs
+++ mix.exs
@@ -47,7 +47,7 @@ defmodule Mockatron.MixProject do
       {:plug_cowboy, "~> 2.0"},
       {:cachex, "~> 3.2.0"},
       {:comeonin, "~> 5.1"},
-      {:bcrypt_elixir, "~> 1.0"},
+      {:bcrypt_elixir, "~> 2.0"},
       {:guardian, "~> 2.0"},
       {:excoveralls, "~> 0.10", only: :test}
     ]
