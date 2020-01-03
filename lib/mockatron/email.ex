17d9a1d98e98f5116a2adb06f88151c2dc66ca95
Rodrigo Zampieri Castilho <rodrigo.zampieri@gmail.com>
2020-01-03 20:59:32 -0300

    Adjust email template and temporary home page

--- lib/mockatron/email.ex
+++ lib/mockatron/email.ex
@@ -5,7 +5,7 @@ defmodule Mockatron.Email do
     new_email()
     |> to(email_address)
     |> from("postmaster@mockatron.io")
-    |> subject("Welcome to Mockatron!")
+    |> subject("Welcome to mockatron.io!!!")
     |> put_html_layout({MockatronWeb.EmailView, "verify_email.html"})
     |> render("verify_email.html", email_address: email_address, verification_url: verification_url)
   end
