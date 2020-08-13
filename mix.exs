8ebb139ac95bda6d87a8eae55203ec0ec685493a
Rodrigo Zampieri Castilho <rodrigo.zampieri@gmail.com>
2020-08-12 23:10:13 -0300

    Update OTP and Elixir releases

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Mockatron.MixProject do
     [
       app: :mockatron,
       version: "0.1.0",
-      elixir: "~> 1.8.2",
+      elixir: "~> 1.10",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
