05ce4548838af3049fed5a29ed084f8b0dee35f1
Rodrigo Zampieri Castilho <rodrigo.zampieri@gmail.com>
2021-08-23 09:38:49 -0300

    Remove livedashboard

--- lib/mockatron_web/endpoint.ex
+++ lib/mockatron_web/endpoint.ex
@@ -35,10 +35,6 @@ defmodule MockatronWeb.Endpoint do
     plug Phoenix.Ecto.CheckRepoStatus, otp_app: :mockatron
   end
 
-  plug Phoenix.LiveDashboard.RequestLogger,
-    param_key: "request_logger",
-    cookie_key: "request_logger"
-
   plug Plug.RequestId
   plug Plug.Telemetry, event_prefix: [:phoenix, :endpoint]
 
